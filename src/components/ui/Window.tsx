a1fd64404edee2677010d23ba4a6c9110b9fb725
Bertrand <aat.eu>
2026-01-13 13:46:07 +0100

    bg header window

--- src/components/ui/Window.tsx
+++ src/components/ui/Window.tsx
@@ -14,7 +14,7 @@ type WindowProps = {
     return (
       <div className={[[" shadow-[inset_2px_2px_0_0_#fff,inset_-2px_-2px_0_0_#000,8px_8px_0_0_rgba(0,0,0,0.5)] bg-gray-300 p-2", className ].join(" "), tailleClassName].join("")}>
         {title ? (
-          <div className={["mb-2  flex items-center justify-between text-white px-3 py-2 shadow-[inset_2px_2px_0_0_#fff,inset_-2px_-2px_0_0_#000]", titleClassName].join(" ")} >
+          <div className={["mb-2  flex items-center justify-between text-white px-3 py-2 bg-blue-900 shadow-[inset_2px_2px_0_0_#fff,inset_-2px_-2px_0_0_#000]", titleClassName].join(" ")} >
             <span aria-hidden="true" className={["font-semibold no-select ", tailleTitle].join("")}>{title}</span>
             {fleche ? (
             <img
