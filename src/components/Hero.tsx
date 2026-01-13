0bae8b42fe75baf8ddb2ca9129e9b316e0f063f9
Bertrand <aat.eu>
2026-01-13 14:23:57 +0100

    d

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -132,7 +132,7 @@ export default function Hero({ data }: Props) {
           </DraggableWindow>
 
           {/* Logo window */}
-          <DraggableWindow defaultPosition={{ x: "70%", y: "30%" }} className="z-40">
+          <DraggableWindow defaultPosition={{ x: "80%", y: "20%" }} className="z-40">
             <LogoSpin />
           </DraggableWindow>
 
